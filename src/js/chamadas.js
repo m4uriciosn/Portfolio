8736f2b58bc03ce5df58b5c713e4312a7dcca7d4
Maurício Sosmaiar Nascimento <105497997+m4uriciosn@users.noreply.github.com>
2023-02-12 00:03:09 -0300

    adiciona botao de download CV

--- src/js/chamadas.js
+++ src/js/chamadas.js
@@ -1,58 +1,74 @@
 // Botão Ver Mais
 
 const escondido = document.getElementById('div-escondido');
-const ver = document.getElementById('ver')
+const ver = document.getElementById('ver');
 
 function vermais() {
-    if (escondido.style.display === "none") {
+    if (escondido.style.display == "none") {
     escondido.style.display='flex';
-    ver.innerHTML='ver menos'
+    ver.innerHTML='ver menos';
     } else {
         escondido.style.display='none';
-        ver.innerHTML='ver mais'
+        ver.innerHTML='ver mais';
     }
 }
 
-// botão responsivo
+// Botões do menu responsivo
 
 function openMenu() {
-    document.querySelector(".menu-responsivo").style.height = '100vh';
+    document.querySelector(".menu-responsivo").style.height = '100%';
+    document.querySelector(".btn-menu").style.display='none';
 }
 
 function fechaMenu() {
     document.querySelector(".menu-responsivo").style.height = '0';
+    document.querySelector(".btn-menu").style.display='block';
 }
 
-// chamada dos botões de redes sociais e contatos
+function removerBtn() {
+    const x = document.querySelector(".menu-responsivo");
+    const btn = document.querySelector(".btn-menu");
+    if (window.innerWidth >= 650) {
+        btn.style.display='none';
+    } else if (window.innerWidth >= 650 || x.style.height=='100%') {
+        btn.style.display='none';
+    } else {
+        btn.style.display='block';
+    }
+}
+
+// Chamada dos botões de redes sociais e contatos
+
+const linkedin = 'https://www.linkedin.com/in/maurício-sosmaiar-nascimento-/';
+const instagram = 'https://instagram-menos-que-original.netlify.app';
+const flex = 'https://flex-transportes.netlify.app';
+const mario = 'https://projeto-mario.netlify.app';
+const git = 'https://github.com/m4uriciosn?tab=repositories';
+const enigma = 'https://pre-alpha-codigo-safe.netlify.app';
+const dark = 'https://projeto-dark-souls.netlify.app';
+const percy = 'https://m4uriciosn.github.io/Percy-Jackson/';
 
 function chamarlinkedin(){
-    window.location = 'https://www.linkedin.com/in/maurício-sosmaiar-nascimento-/'
+    window.location = linkedin;
 }
-
 function chamarInstagram(){
-    window.location = 'https://www.instagram.com/m4uriciosn/'
+    window.location = instagram;
 }
-
 function chamarFlex(){
-    window.location = 'https://flex-transportes.netlify.app'
+    window.location = flex;
 }
-
 function chamarMario(){
-    window.location = 'https://projeto-mario.netlify.app'
+    window.location = mario;
 }
-
 function chamarGithub(){
-    window.location = 'https://github.com/m4uriciosn?tab=repositories'
+    window.location = git;
 }
-
 function chamarEnigma(){
-    window.location = 'https://pre-alpha-codigo-safe.netlify.app'
+    window.location = enigma;
 }
-
 function chamarDark(){
-    window.location = 'https://projeto-dark-souls.netlify.app'
+    window.location = dark;
 }
-
 function chamarPercy() {
-    window.location = 'https://m4uriciosn.github.io/Percy-Jackson/'
+    window.location = percy;
 }
